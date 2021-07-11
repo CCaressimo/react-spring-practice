08e6925ba53e815785607527c8685f84dc7aea2d
chris <c.caressimo@gmail.com>
2021-07-11 14:22:35 -0400

    fade in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React from 'react';
+import { useSpring, animated } from 'react-spring'
+import { BrowserRouter,Switch , Router } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const fade = useSpring({
+    from: {
+      opacity: 0
+    },
+    to: {
+      opacity: 1
+    }
+  });
+
   return (
-    <div className="App">
-      <h1>ahh</h1>
-    </div>
+    <animated.div className="App" style={fade}>
+      <header className="App-header">
+        <h1>AHHHHH</h1>
+        <button className="menu-button">Menu</button>
+      </header>
+    </animated.div>
   );
 }
 
